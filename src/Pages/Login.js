08c144090f0f341cb0ddaa011347a0a11a8191ea
GiovaniKill <giovanikill@hotmail.com>
2022-06-28 17:13:15 -0400

    group-10-req-4

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import logo from '../trivia.png';
 
 const MINIMUN_LENTGH = 3;
