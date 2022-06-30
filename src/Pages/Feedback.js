04c2e567166b87b0f1c2d224f282388ed97aba7d
Igor Brizack <igor_brizack@hotmail.com>
2022-06-30 14:21:53 -0300

    group 10 req 13

--- src/Pages/Feedback.js
+++ src/Pages/Feedback.js
@@ -3,12 +3,32 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../Components/Header';
 
+const ASSERTION3 = 3;
+
 class Feedback extends React.Component {
+  state = {
+    feedbackMsg: '',
+  }
+
+  componentDidMount() {
+    this.checkAssertions();
+  }
+
+  checkAssertions = () => {
+    const { assertions } = this.props;
+    if (assertions < ASSERTION3) {
+      this.setState({ feedbackMsg: 'Could be better...' });
+    } else {
+      this.setState({ feedbackMsg: 'Well Done!' });
+    }
+  }
+
   render() {
+    const { feedbackMsg } = this.state;
     return (
       <div>
-        <h1 data-testid="feedback-text">Feedback</h1>
         <Header />
+        {feedbackMsg && <p data-testid="feedback-text">{feedbackMsg}</p>}
       </div>
     );
   }
@@ -18,12 +38,14 @@ const mapStateToProps = (globalState) => ({
   gravatarEmail: globalState.player.gravatarEmail,
   name: globalState.player.name,
   score: globalState.player.score,
+  assertions: globalState.player.assertions,
 });
 
 Feedback.propTypes = {
   gravatarEmail: PropTypes.string,
   name: PropTypes.string,
   player: PropTypes.number,
+  assertions: PropTypes.number,
 }.isRequired;
 
 export default connect(mapStateToProps)(Feedback);
