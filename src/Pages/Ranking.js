6c38ab44ee0390bad7916d5e2f6345869d2d2c0c
GiovaniKill <giovanikill@hotmail.com>
2022-06-30 14:53:01 -0400

    group-10-req-18

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
     return (
       <div>
         <p data-testid="ranking-title">Ranking</p>
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">Tela Inicial</button>
+        </Link>
       </div>
     );
   }
