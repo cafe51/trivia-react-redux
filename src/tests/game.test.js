e8e5a3d2c4cb50dce99e6e5dcb16e4e1730a6c59
Igor Brizack <igor_brizack@hotmail.com>
2022-07-01 19:38:26 -0300

    group 10 req 21-2

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -25,4 +25,28 @@ describe('Testando se a página de game...', () => {
 
     expect(history.location.pathname).toBe('/');
   })
+
+  test('verifica se os botões desabilitam ao tempo chegar em 0', async () => {
+    jest.clearAllMocks()
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const nameInput = screen.getByRole('textbox', { name:/nome:/i });
+    const emailInput = screen.getByRole('textbox', { name:/email:/i });
+    const playButton = screen.getByRole('button', { name:/play/i });
+
+    userEvent.type(nameInput, 'grupo10')
+    userEvent.type(emailInput, 'grupo10@hotmail.com')
+    userEvent.click(playButton);
+
+    expect(await screen.findByText(/^27$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^21$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^16$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^11$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^6$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^1$/i, {},{timeout: 5000})).toBeInTheDocument()
+    expect(await screen.findByText(/^0$/i, {},{timeout: 5000})).toBeInTheDocument()
+    const correctAnswer1 = screen.getByTestId('correct-answer');
+
+    expect(correctAnswer1).toBeDisabled()
+  })
 })
